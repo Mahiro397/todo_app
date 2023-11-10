ced88295e748640a9edc173961ea82a923a298e4
mahiro397 <397zakiyama3150@gmail.com>
2023-11-10 11:14:20 +0900

    routerを経由してページを表示

--- resources/js/pages/Example.jsx
+++ resources/js/pages/Example.jsx
@@ -9,7 +9,7 @@ function Example() {
                     <div className="card">
                         <div className="flex flex-col items-center gap-4 md:gap-6 bg-red-50">こんにちは</div>
                         <div className="flex flex-col items-center gap-4 md:gap-6 bg-green-100">おはようございます。</div>
-                        <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>ボタンをクリック</button>
+                        <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>ボタンをクリックsina</button>
                     </div>
                 </div>
             </div>
